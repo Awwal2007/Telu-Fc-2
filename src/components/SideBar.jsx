c7e3cf7c109c06afd64ee23c5804d530357dc7aa
Awwal2007 <awwalsaminu9@gmail.com>
2025-12-14 21:12:47 +0100

    added favicon and some things

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,20 +6,11 @@ import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 
 import { SlCalender } from "react-icons/sl";
-import useMediaQuery from './MediaQuery';
 import FacebookPosts from './FacebookPosts';
 
 const SideBar = () => {  
-//   const isMobile = useMediaQuery('(maxWidth: 175px)')
   return (
     <div style={{width: "100%",gap: "30px", display: "flex", flexDirection: "column"}}>
-        {/* <div>
-            <p>Upcoming Events</p>
-        </div>
-        <div style={{display: "flex", alignItems: "center"}}>
-            <div style={{justifyContent: "center"}}><SlCalender size={25}/></div>
-            <div><span style={{marginLeft: "10px", fontSize: "12px"}}>There are no upcoming events</span></div>
-        </div> */}
         <div>
             {/* <FacebookWidget /> */}
             <FacebookPosts />
